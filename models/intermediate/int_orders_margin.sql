572a5b7e4935027f5dcb01df9d99cbcccd58c365
Igor T. <trindadeigu@gmail.com>
2024-09-07 20:13:25 +0000

    creation of orders margin

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,8 @@
+SELECT
+    orders_id
+    ,date_date
+    ,ROUND(SUM(revenue),2) AS revenue
+    ,ROUND(SUM(purchase_cost),2) AS purchase_cost
+    ,ROUND(SUM(margin),2) AS margin
+FROM {{ ref('int_sales_margin') }}
+GROUP BY orders_id, date_date
